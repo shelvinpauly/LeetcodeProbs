5c3c232fbd9593189029ac6394e75e2b2d7d83ec
Shelvin Pauly <spauly@terpmail.umd.edu>
2024-02-05 03:16:17 -0800

    .

--- Trees/max_depth_bin_tree.py
+++ Trees/max_depth_bin_tree.py
@@ -23,3 +23,42 @@
 
 # The number of nodes in the tree is in the range [0, 104].
 # -100 <= Node.val <= 100
+
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         level = 0
+#         q = deque([root])
+#         while q:
+#             for i in range(len(q)):
+#                 TreeNode = q.popleft()
+#                 if TreeNode.left:
+#                     q.append(TreeNode.left)
+#                 if TreeNode.right:
+#                     q.append(TreeNode.right)
+#             level+=1
+#         return level
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
